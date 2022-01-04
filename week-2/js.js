86d599e2fd821ba0d6f1e66a8bb5fb30cf16c97b
tony <tony@gmail.com>
2022-01-04 20:06:34 +0800

    week2 asignment v1

---
+++ week-2/js.js
@@ -0,0 +1,95 @@
+// 作業一
+function calculate(min, max) {
+  // 請用你的程式補完這個函式的區塊
+  let result = 0;
+  for (min; min <= max; min++) {
+    result += min;
+  }
+  //   return result;
+  console.log(result);
+}
+
+calculate(1, 3); // 你的程式要能夠計算 1+2+3，最後印出 6
+calculate(4, 8); // 你的程式要能夠計算 4+5+6+7+8，最後印出 30
+// console.log(calculate(4, 8));
+
+// 作業二
+function avg(data) {
+  // 請用你的程式補完這個函式的區塊，完成以下函式，正確計算出員工的平均薪資，請考慮員工數量會變動的情況。
+  let result = 0;
+  let avg;
+  let employee_count = data.count;
+  console.log(employee_count);
+  for (let i = 0; i < data.employees.length; i++) {
+    result += data.employees[i].salary;
+    // console.log(result);
+  }
+  //   console.log(result);
+  avg = result / employee_count;
+  //   return avg;
+  console.log(avg);
+}
+avg({
+  count: 3,
+  employees: [
+    {
+      name: "John",
+      salary: 30000,
+    },
+    {
+      name: "Bob",
+      salary: 60000,
+    },
+    {
+      name: "Jenny",
+      salary: 50000,
+    },
+  ],
+}); // 呼叫 avg 函式
+
+//作業三 找出至少包含兩筆整數的列表 (Python) 或陣列 (JavaScript) 中，兩兩數字相乘後的最大值。
+function maxProduct(nums) {
+  // 請用你的程式補完這個函式的區塊
+  let arr = [];
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (j != i) {
+        result = nums[i] * nums[j];
+        arr.push(result);
+      }
+    }
+  }
+  //   let max = Math.max.apply(null, arr);
+  //   console.log(max);
+  //try兩兩比對比大小
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    let cur = arr[i];
+    cur > max ? (max = cur) : null;
+  }
+  console.log(max);
+}
+
+maxProduct([5, 20, 2, 6]); // 得到 120
+maxProduct([10, -20, 0, 3]); // 得到 30
+maxProduct([-1, 2]); // 得到 -2
+maxProduct([-1, 0, 2]); // 得到 0
+
+// 作業四
+
+//雙迴圈
+function twoSum(nums, target) {
+  // your code here
+  let sum = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      if (target == nums[i] + nums[j]) {
+        sum.push(i, j);
+      }
+    }
+    return sum;
+  }
+}
+let result = twoSum([2, 11, 7, 15], 9);
+console.log(result); // show [0, 2] because nums[0]+nums[2] is 9
---
+++ week-2/py.py
@@ -0,0 +1,94 @@
+def calculate(min, max):
+    result = 0
+    for i in range(min, max+1):
+        result += i
+    # return(result)
+    print(result)
+
+
+    # 請用你的程式補完這個函式的區塊
+calculate(1, 3)  # 你的程式要能夠計算 1+2+3，最後印出 6
+calculate(4, 8)  # 你的程式要能夠計算 4+5+6+7+8，最後印出 30
+
+
+# 作業二
+def avg(data):
+    employee_count = len(data["employees"])
+    # print(employee_count)
+    # print(len(data["employees"]))
+    # print(data["employees"])
+    result = 0
+    avg = 0
+    for d in data["employees"]:
+        result += d["salary"]
+        # print(d["salary"])
+    # print(result)
+    avg = result/employee_count
+    print(avg)
+
+    # 請用你的程式補完這個函式的區塊
+avg({
+    "count": 3,
+    "employees": [
+        {
+            "name": "John",
+            "salary": 30000
+        },
+        {
+            "name": "Bob",
+            "salary": 60000
+        },
+        {
+            "name": "Jenny",
+            "salary": 50000
+        },
+    ]
+})  # 呼叫 avg 函式
+
+# 作業三
+
+
+def maxProduct(nums):
+    arr = []
+    for i in nums:
+        for j in nums:
+            if(i != j):
+                result = i * j
+                arr.append(result)
+        # print(arr)
+    # maxNum = max(arr)
+    # print(maxNum)
+
+    # try兩兩比對比大小
+    maxnum = arr[0]
+    for x in arr:
+        cur = x
+        if cur > maxnum:
+            maxnum = x
+
+    print(maxnum)
+
+    # 請用你的程式補完這個函式的區塊
+maxProduct([5, 20, 2, 6])  # 得到 120
+maxProduct([10, -20, 0, 3])  # 得到 30
+maxProduct([-1, 2])  # 得到 -2
+maxProduct([-1, 0, 2])  # 得到 0
+
+
+# 作業四
+
+def twoSum(nums, target):
+    result_dit = dict()
+    i = 0
+    while i < len(nums):
+        if (target - nums[i]) not in result_dit:
+            result_dit[nums[i]] = i
+            # print(result_dit)
+            i += 1
+        else:
+            return [result_dit[target-nums[i]], i]
+
+
+# your code here
+result = twoSum([2, 11, 7, 15], 9)
+print(result)  # show [0, 2] because nums[0]+nums[2] is 9
