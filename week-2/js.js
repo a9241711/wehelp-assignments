0420c16fc3329ee22382374d582aa40576402c42
tony <tony@gmail.com>
2022-01-09 21:37:39 +0800

    finish HW5

--- week-2/js.js
+++ week-2/js.js
@@ -1,10 +1,11 @@
 // 作業一
 function calculate(min, max) {
   // 請用你的程式補完這個函式的區塊
-  let result = 0;
-  for (min; min <= max; min++) {
-    result += min;
-  }
+  //   let result = 0;
+  //   for (min; min <= max; min++) {
+  //     result += min;
+  //   }
+  let result = ((min + max) * (max-min+1)) / 2;
   //   return result;
   console.log(result);
 }
@@ -50,32 +51,77 @@ avg({
 //作業三 找出至少包含兩筆整數的列表 (Python) 或陣列 (JavaScript) 中，兩兩數字相乘後的最大值。
 function maxProduct(nums) {
   // 請用你的程式補完這個函式的區塊
-  let arr = [];
-  let result = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (j != i) {
-        result = nums[i] * nums[j];
-        arr.push(result);
+  //時間複雜度O(n^2)，取執行次數中最高次方或最大指數部份，用了兩個迴圈，所以是n*n的次數
+  // let arr = [];
+  // let result = 0;
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = 0; j < nums.length; j++) {
+  //     if (j != i) {
+  //       result = nums[i] * nums[j];
+  //       arr.push(result);
+  //     }
+  //   }
+  // }
+  // //   let max = Math.max.apply(null, arr);
+  // //   console.log(max);
+  // //try兩兩比對比大小
+  // let max = arr[0];
+  // for (let i = 1; i < arr.length; i++) {
+  //   let cur = arr[i];
+  //   cur > max ? (max = cur) : null;
+  // }
+  // console.log(max);
+
+  //方法二//Sort寫法
+
+//   nums.sort(function(a,b){
+//     return a-b;
+//   })
+//   let newarr =nums.slice(-2)
+//   let result= newarr[0]*newarr[1]
+//   console.log(result)
+// }
+
+///方法三，選擇排序
+    
+    // for(let i=0;i<nums.length;i++){
+    //   let min_num= nums[i];
+    //   let min_index=i;
+    //   for(let j=i; j<nums.length-1; j++){
+    //     if(min_num > nums[j]){
+    //       min_num =nums[j];
+    //       min_index = j;
+    //     }
+    //   }[nums[min_index],nums[i]]=[nums[i],nums[min_index]]; 
+    //   //第一次[2:5,0:2]//第二次[2:20,1:5]//第三次[3:20,2:6]
+    // }console.log(nums);
+    // let twoMax= nums.slice(-2);
+    // let result = twoMax[0]*twoMax[1];
+    // console.log(result)
+
+    //方法四插入法
+
+    for(let i =1;i<nums.length;i++){
+      let position =i;
+      let value = nums[i]
+      while( position>=0 && value <nums[position-1]){
+        console.log('i:',position,nums[position],value);
+        [nums[position],nums[position-1]]=[nums[position-1],nums[position]]
+        position--;
+        console.log(position,nums,value)
       }
+      console.log("ANY"+nums[position])
+      // nums[position]=value;
+      console.log(nums)
+     
     }
   }
-  //   let max = Math.max.apply(null, arr);
-  //   console.log(max);
-  //try兩兩比對比大小
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    let cur = arr[i];
-    cur > max ? (max = cur) : null;
-  }
-  console.log(max);
-}
-
-maxProduct([5, 20, 2, 6]); // 得到 120
-maxProduct([10, -20, 0, 3]); // 得到 30
-maxProduct([-1, 2]); // 得到 -2
-maxProduct([-1, 0, 2]); // 得到 0
-maxProduct([-1, -2, 0]);
+maxProduct([50, 20, 2, 6]); // 得到 120
+maxProduct([5, 20, 200, 600,6700,6800]); 
+// maxProduct([10, -20, 0, 3]); // 得到 30
+// maxProduct([-1, 2]); // 得到 -2
+// maxProduct([-1, 0, 2]); // 得到 0
+// maxProduct([-1, -2, 0]);
 
 // 作業四
 
@@ -90,17 +136,81 @@ function twoSum(nums, target) {
   //       }
   //     }
   //     return sum;
-  //   }
-  let sum = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > target) {
-      continue;
+  //   }[2, 11, 7, 15], 9)
+    let arr=[];
+    for (let i=0; i<nums[i];i++){
+      if( nums[i]> target){
+        continue;
+      }
+      if(nums.includes(target-nums[i])){
+        arr.push(i)
+      }
+    console.log(arr)
     }
-    if (nums.includes(target - nums[i])) {
-      sum.push(i);
-    }
-  }
-  return sum;
 }
+
+//   let sum = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > target) {
+//       continue;
+//     }
+//     if (nums.includes(target - nums[i])) {
+//       sum.push(i);
+//     }
+//   }
+//   return sum;
+// }
 let result = twoSum([2, 11, 7, 15], 9);
 console.log(result); // show [0, 2] because nums[0]+nums[2] is 9
+
+//作業五
+
+function maxZeros(nums) {
+  // 請用你的程式補完這個函式的區塊
+  // let arr = [];
+  // let count = 0;
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] == 0) {
+  //     count++;
+  //   } else if (nums[i] == 1) {
+  //     arr.push(count);
+  //     count = 0;
+  //   } else {
+  //     if (count != 0) {
+  //       arr.push(count);
+  //       count = 0;
+  //     }
+  //   }
+  //   if (count != 0) {
+  //     arr.push(count);
+  //   }
+  // }
+  // //   let max = Math.max.apply(null, arr);
+  // //   console.log(max);
+  // let max = arr[0];
+  // for (let i = 0; i < arr.length; i++) {
+  //   let cur = arr[i];
+  //   if (cur > max) {
+  //     max = cur;
+  //   } else {
+  //     null;
+  //   }
+  // }
+  // console.log(max);
+  let temp=0;
+  let max =0;
+  for (let i =0; i<nums.length;i++){
+    if(nums[i]==0){
+      temp +=1;
+    }else{
+      temp=0
+    }
+    if(temp>max){
+      max=temp
+    }
+  }console.log(max)
+}
+maxZeros([0, 1, 0, 0]); // 得到 2
+maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0]); // 得到 4
+maxZeros([1, 1, 1, 1, 1]); // 得到 0
+maxZeros([0, 0, 0, 1, 1]); // 得到 3
--- week-2/py.py
+++ week-2/py.py
@@ -5,7 +5,6 @@ def calculate(min, max):
     # return(result)
     print(result)
 
-
     # 請用你的程式補完這個函式的區塊
 calculate(1, 3)  # 你的程式要能夠計算 1+2+3，最後印出 6
 calculate(4, 8)  # 你的程式要能夠計算 4+5+6+7+8，最後印出 30
@@ -25,6 +24,7 @@ def avg(data):
     # print(result)
     avg = result/employee_count
     print(avg)
+
 
     # 請用你的程式補完這個函式的區塊
 avg({
@@ -47,6 +47,9 @@ avg({
 
 # 作業三
 
+# 時間複雜度 O(n^2)
+# 取執行次數中最高次方或最大指數部份
+
 
 def maxProduct(nums):
     arr = []
@@ -68,6 +71,7 @@ def maxProduct(nums):
 
     print(maxnum)
 
+
     # 請用你的程式補完這個函式的區塊
 maxProduct([5, 20, 2, 6])  # 得到 120
 maxProduct([10, -20, 0, 3])  # 得到 30
@@ -79,17 +83,46 @@ maxProduct([-1, -2, 0])
 # 作業四
 
 def twoSum(nums, target):
-    result_dit = dict()
+    # result_dit = dict()
+    # i = 0
+    # while i < len(nums):
+    #     if (target - nums[i]) not in result_dit:
+    #         result_dit[nums[i]] = i
+    #         # print(result_dit)
+    #         i += 1
+    #     else:
+    #         return [result_dit[target-nums[i]], i]
+    result = {}
     i = 0
-    while i < len(nums):
-        if (target - nums[i]) not in result_dit:
-            result_dit[nums[i]] = i
-            # print(result_dit)
-            i += 1
-        else:
-            return [result_dit[target-nums[i]], i]
+    for i in range(len(nums)):
+        if nums[i] > target:
+            continue
+        if target - nums[i] in nums:
+            result[nums[i]] = i
+    return(result.values(), result)
 
 
 # your code here
 result = twoSum([2, 11, 7, 15], 9)
 print(result)  # show [0, 2] because nums[0]+nums[2] is 9
+
+
+# 作業五
+
+def maxZeros(nums):
+    temp = 0
+    max = 0
+    for i in nums:
+        if(i == 0):
+            temp += 1
+        else:
+            temp = 0
+        if temp > max:
+            max = temp
+    print(max)
+
+    # 請用你的程式補完這個函式的區塊
+maxZeros([0, 1, 0, 0])  # 得到 2
+maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0])  # 得到 4
+maxZeros([1, 1, 1, 1, 1])  # 得到 0
+maxZeros([0, 0, 0, 1, 1])  # 得到 3
