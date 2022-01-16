81769241b5fe4ac9ee08cadbef4d00c076c10996
tony <tony@gmail.com>
2022-01-16 15:29:24 +0800

    rewrite something

--- week-3/js.js
+++ week-3/js.js
@@ -1,100 +1,142 @@
-function get(url){
-    return new Promise((resolve,reject)=>{
-        //定義http
-    let req= new XMLHttpRequest();
-    req.open("GET", url , true);
-    req.onload=function(){
-        if (req.status==200){
-            resolve(JSON.parse(req.response))
-        }else{
-            reject(new Error(req))
-        }
-    };
-    req.send();
-    })
+// function get(url){
+//     return new Promise((resolve,reject)=>{
+//         //定義http
+//     let req= new XMLHttpRequest();
+//     req.open("GET", url , true);
+//     req.onload=function(){
+//         if (req.status==200){
+//             resolve(JSON.parse(req.response))
+//         }else{
+//             reject(new Error(req))
+//         }
+//     };
+//     req.send();
+//     })
 
-}
+// }
 
-// let content = document.querySelector(".container-content")
-get("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then((res)=>{
-    // console.log(res.result.results)
-    let data = res["result"]["results"];
-    let text = document.querySelectorAll(".text");
+// // let content = document.querySelector(".container-content")
+// get("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then((res)=>{
+//     // console.log(res.result.results)
+//     let data = res["result"]["results"];
+//     let text = document.querySelectorAll(".text");
     
-    //處理圖片
-    for(let i=0;i<text.length; i++){
+//     //處理圖片
+//     for(let i=0;i<text.length; i++){
         
-        //處理圖片
-        let img= document.createElement("img");
-        let imgurl ="https://"+ data[i]["file"].toLowerCase().split("https://")[1];
-         //處理文字
+//         //處理圖片
+//         let img= document.createElement("img");
+//         let imgurl ="https://"+ data[i]["file"].toLowerCase().split("https://")[1];
+//          //處理文字
+//         let p = document.createElement("p")
+//         let p_value=document.createTextNode(data[i]["stitle"])
+//         p.appendChild(p_value);
+//         img.setAttribute("src", imgurl);
+//         text[i].appendChild(img);
+//         text[i].appendChild(p);
+        
+
+//         // console.log(imgurl);
+//     }
+//     //載入處理按鈕
+//     let btn = document.querySelector(".btn")
+//     let container = document.querySelector(".container");
+    
+//     btn.addEventListener("click",()=>{
+//         let maxNum=data.length
+//         // console.log(textNum)
+//         let textall=document.querySelectorAll(".text");
+//         let currentNum=textall.length    
+//         for( let i= currentNum; i<currentNum+8;i++){
+//             // console.log(currentNum)
+//             if(i>maxNum){
+//                break;
+//             }
+//             //處理圖片
+//             let img= document.createElement("img");
+//             let imgurl ="https://"+ data[i]["file"].toLowerCase().split("https://")[1];
+//             //處理文字
+//             let p = document.createElement("p")
+//             let p_value=document.createTextNode(data[i]["stitle"])
+//             //新增div
+//             let newdiv= document.createElement("div");
+//             newdiv.setAttribute("class","text");
+//             p.appendChild(p_value);
+//             img.setAttribute("src", imgurl);
+//             newdiv.appendChild(img)
+//             newdiv.appendChild(p);
+//             container.appendChild(newdiv);         
+//             // console.log(currentNum)
+//         }
+//         currentNum =textall.length;
+//         console.log(currentNum);
+
+//     })
+   
+// }).catch((res)=>{
+//     console.error(res)
+// })
+
+//練習傳統xhr作法
+// let xhr = new XMLHttpRequest();
+// let url ="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json";
+// xhr.open("GET", url, true);
+// xhr.send();
+// //onload表示readystate=4後執行funciton
+// xhr.onload= function(){
+//     let response= JSON.parse(xhr.responseText);
+//     let data =response["result"]["results"];
+//     console.log(data)
+// }
+
+
+
+//fetch練習
+let url ="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json";
+fetch(url).then((res)=>{
+    return res.json();
+}).then((data)=>{
+    // console.log(data);
+    result=data["result"]["results"];
+    // console.log(result);
+    let text = document.querySelectorAll(".text");
+    let container = document.querySelector(".container");
+    for (let i=0; i<8;i++){
+        // console.log(result[i])
+        // let stitle =result[i]["stitle"];
+        let file="https://"+result[i]["file"].split("https://")[1].toLowerCase();
+        let img = document.createElement("img");
         let p = document.createElement("p")
-        let p_value=document.createTextNode(data[i]["stitle"])
+        let p_value=document.createTextNode(result[i]["stitle"])
         p.appendChild(p_value);
-        img.setAttribute("src", imgurl);
+        img.setAttribute("src",file);
         text[i].appendChild(img);
         text[i].appendChild(p);
-        
-
-        // console.log(imgurl);
     }
-    //載入處理按鈕
-    let btn = document.querySelector(".btn")
-    let container = document.querySelector(".container");
-    
+    let btn= document.querySelector(".btn");
     btn.addEventListener("click",()=>{
-        let maxNum=data.length
-        // console.log(textNum)
-        let textall=document.querySelectorAll(".text");
-        let currentNum=textall.length    
-        for( let i= currentNum; i<currentNum+8;i++){
-            // console.log(currentNum)
-            if(i>maxNum){
-               break;
+        let maxnum=result.length;
+        let currentNum=  document.querySelectorAll(".text").length
+        console.log(currentNum)
+        for(let i =currentNum;i<currentNum+8;i++){
+            if(i>maxnum){
+                break;
             }
-            //處理圖片
-            let img= document.createElement("img");
-            let imgurl ="https://"+ data[i]["file"].toLowerCase().split("https://")[1];
-            //處理文字
-            let p = document.createElement("p")
-            let p_value=document.createTextNode(data[i]["stitle"])
-            //新增div
-            let newdiv= document.createElement("div");
-            newdiv.setAttribute("class","text");
+            let newdiv =document.createElement("div");
+            newdiv.setAttribute("class", "text");
+            let file="https://"+result[i]["file"].split("https://")[1].toLowerCase();
+            let p = document.createElement("p");
+            let p_value=document.createTextNode(result[i]["stitle"])
+            let img = document.createElement("img");
             p.appendChild(p_value);
-            img.setAttribute("src", imgurl);
-            newdiv.appendChild(img)
+            img.setAttribute("src",file)
+            newdiv.appendChild(img);
             newdiv.appendChild(p);
-            container.appendChild(newdiv);         
-            // console.log(currentNum)
-        }
-        currentNum =textall.length;
-        console.log(currentNum);
-
+            container.appendChild(newdiv);
+        } 
     })
    
-}).catch((res)=>{
-    console.error(res)
-})
 
-// xhr.send();
-// xhr.onload=function(){
-//     console.log(xhr)
-//     if(this.readyState ===4 && this.status ==200){
-//     let response = JSON.parse(this.response);
-//     // console.log(response.result.results[0]);
-//     let data = response.result;
-    
-    // for (let i=0;i<7;i++){
-    //     h1.textContent= data.results[i].stitle;
-    //     text.appendChild(h1)
-    //     // console.log(title)
-    // }
-    
-    
-    // img.setAttribute("src", data.results[0].file)
-    
-//     }else{
-//     console.log("error")
-//     }
-// }
+}).catch((e)=>{
+    console.log(e);
+})
--- week-3/py.py
+++ week-3/py.py
@@ -1,25 +1,52 @@
+# import urllib.request as request
+# import json
+# import csv
+
+# src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+# with request.urlopen(src) as response:
+#     data = json.load(response)  # 透過Json模組處理json格式
+# # print(data)
+
+# # 取得要的資料景點名稱,區域,經度,緯度,第一張圖檔網址
+# list = data["result"]["results"]
+# header = ["地點", "地址", "經度", "緯度", "圖片"]
+
+# # 轉換成csv
+# with open("data.csv", "w", newline='', encoding="utf-8-sig") as file:
+#     csv_output = csv.DictWriter(file, fieldnames=header)
+#     csv_output.writeheader()
+#     for item in list:
+#         title = item["stitle"]
+#         address = item["address"][5:8]
+#         longitude = item["longitude"]
+#         latitude = item["latitude"]
+#         files = "https://" + item["file"].split("https://")[1]
+#         # print(title, address, files)
+#         file.write(title+","+address+","+longitude+","+latitude+","+files+"\n")
+
+
+# 練習復刻上方程式碼
+from encodings import utf_8_sig
 import urllib.request as request
 import json
 import csv
 
-src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
-with request.urlopen(src) as response:
-    data = json.load(response)  # 透過Json模組處理json格式
-# print(data)
+url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+with request.urlopen(url) as request:  # urlopen，因為來源是JSON格式的url
+    data = json.load(request)
 
-# 取得要的資料景點名稱,區域,經度,緯度,第一張圖檔網址
-list = data["result"]["results"]
-header = ["地點", "地址", "經度", "緯度", "圖片"]
 
-# 轉換成csv
-with open("data.csv", "w", newline='', encoding="utf-8-sig") as file:
-    csv_output = csv.DictWriter(file, fieldnames=header)
-    csv_output.writeheader()
+with open("csv2.csv", "w", newline="",  encoding="utf_8_sig") as file:
+    list = data["result"]["results"]
+    # print(list)
+    header = ["景點", "城市", "經度", "緯度", "照片"]
+    writer = csv.writer(file)
+    writer.writerow(header)
     for item in list:
-        title = item["stitle"]
-        address = item["address"][5:8]
-        longitude = item["longitude"]
+        stitle = item["stitle"]
+        # print(stitle)
+        city = item["address"][4:8]
         latitude = item["latitude"]
-        files = "https://" + item["file"].split("https://")[1]
-        # print(title, address, files)
-        file.write(title+","+address+","+longitude+","+latitude+","+files+"\n")
+        longitude = item["longitude"]
+        files = "https://" + item["file"].split("https://")[1].lower()
+        file.write(stitle+","+city+","+latitude+","+longitude+","+files+"\n")
