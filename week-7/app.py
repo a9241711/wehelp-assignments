4e65d71518001ac87ce55a03974fc4fe197cf599
tony <tony@gmail.com>
2022-02-17 22:12:58 +0800

    HW7 V1

---
+++ week-7/app.py
@@ -0,0 +1,172 @@
+from urllib import response
+from flask import Flask, flash, redirect, url_for, request, render_template, session,jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector,json
+from mysql.connector import pooling,Error
+from flask_restful import Resource, Api
+
+try:
+    connection_pool=pooling.MySQLConnectionPool(
+        pool_name="test_pool",
+        pool_size=20,
+        pool_reset_session=True,
+        host="localhost",
+        database="member",
+        user="root",
+        password="root",
+        )
+    
+    
+    # print(connection_pool.pool_name)
+    # print(connection_pool.pool_size)
+    #連結connection_pool
+    # connection=connection_pool.get_connection()
+    # mycursor=connection.cursor()
+except Error as e:
+    print("Error while connecting to MySQL using Connection pool ", e)
+# finally:
+#     # closing database connection.
+#     if connection.is_connected():
+#         mycursor.close()
+#         connection.close()
+#         print("MySQL connection is closed")
+# 建立Application物件
+app = Flask(__name__)
+#建立Database
+# mydb = mysql.connector.connect(
+#  host="localhost",
+#  user="root",
+#  password="root",
+#  database="member"
+#     )
+api= Api(app) #創建api物件
+# mycursor = mydb.cursor()
+app.secret_key = "mySecret"  # session的密鑰
+app.config["PERMANENT_SESSION_LIFETIME"] = 600
+
+ 
+class Search(Resource):
+    def get(self):
+        username=request.args.get("username")
+        connection=connection_pool.get_connection()
+        mycursor=connection.cursor()
+        mycursor.execute("SELECT id,name,username from member WHERE username=%s",(username,))
+        user=mycursor.fetchone()
+        connection.close()#關閉connection pool
+        if user:
+            return {"data": {
+                "id":user[0],
+                "name":user[1],
+                "username":user[2]
+                    }
+                }
+        else:
+            return{"data":None
+            }
+    def post(self):
+        if session.get("username"):
+            username=session.get("username")
+            name =session.get("name")
+            newname=request.get_json()
+            print(newname["name"])
+            if newname["name"] =='' or newname["name"] == name:
+                res={"error":"true"}
+                print(res)
+                return jsonify(res)
+            else:
+                connection=connection_pool.get_connection()
+                mycursor=connection.cursor()
+                update ="UPDATE member SET name=%s WHERE username=%s"
+                val = (newname["name"],username)
+                mycursor.execute(update,val)
+                connection.commit()
+                session["name"]=newname["name"]
+                connection.close()#關閉connection pool
+                return jsonify( {"ok":"true"})
+        else:
+            res={"error":"true"}
+            print(res)
+            return jsonify(res)
+api.add_resource(Search, "/api/members")
+
+
+@app.route("/")
+def index():
+    successMssage=request.args.get("successMssage")
+    return render_template("index.html",successMssage=successMssage)
+
+
+@app.route("/signin", methods=["GET","POST"])
+def signin():
+    if request.method =="POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        connection=connection_pool.get_connection()
+        mycursor=connection.cursor() 
+        mycursor.execute("SELECT name,username,password FROM member WHERE username=%s",(username,))
+        user=mycursor.fetchone()
+        connection.close()#關閉connection pool
+        # print(user)
+        if username == "" or password == "":
+            flash('請輸入帳號或密碼')
+            return redirect(url_for("index"))
+        if not user :
+            message="帳號或密碼錯誤"
+            return redirect(url_for("error",message=message))
+        userpassword=user[2] #取得使用者密碼
+        if not check_password_hash(userpassword,password):#驗證密碼
+            message="帳號或密碼錯誤"
+            return redirect(url_for("error",message=message))
+        #if user exist
+        session['name'] = user[0]  # 使用者存入session
+        session['username']=user[1]
+        print(user[0],user[1])
+        flash('You were successfully logged in')
+        return redirect(url_for("member"))
+    return render_template("index.html")
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method== "POST":
+        name =request.form["name"]
+        username = request.form["username"]
+        # password = request.form["password"]
+        hashed_password = generate_password_hash(request.form["password"])
+        connection=connection_pool.get_connection()  #連結connection pool
+        mycursor=connection.cursor()
+        mycursor.execute("SELECT username FROM member WHERE username=%s",(username,))
+        finduser = mycursor.fetchone()
+        
+        # print(finduser)
+        if finduser:
+            message="帳號已被註冊"
+            return redirect(url_for("error",message=message))
+        register=("INSERT INTO member (name,username,password) VALUES (%s,%s,%s)")
+        val=(name,username,hashed_password)
+        mycursor.execute(register,val)
+        connection.commit()#存入DB
+        connection.close()#關閉connection pool
+        successMssage="註冊成功"
+    return redirect(url_for("index",successMssage=successMssage))
+
+
+@app.route("/error")
+def error():
+    responseMessage = request.args.get("message")
+    return render_template("error.html", errorMessage=responseMessage)
+
+
+@app.route("/member")
+def member(): 
+    return render_template("member.html", name=session.get("name"),username=session.get("username"))
+
+
+@app.route("/signout")
+def signout():
+    session.pop('username', None)
+    return redirect(url_for("index"))
+
+
+app.config["DEBUG"] = True
+if __name__ == '__main__':
+    app.run(port=3000)
---
+++ week-7/static/js.js
@@ -0,0 +1,51 @@
+
+let search=document.querySelector(".search")
+let inputchange =document.querySelector(".inputchange")
+search.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let txt=document.querySelector(".txt").value;
+    let showperson = document.querySelector(".showperson")
+    let showresult =document.querySelector(".showresult")
+    fetch(`http://127.0.0.1:3000/api/members?username=${txt}`).then((response)=>{
+        return response.json();
+    }).then((myJson)=>{
+        result=myJson.data.name //練習用.取值
+        showperson.textContent=result
+        showresult.append(showperson)
+        // console.log(showperson)
+    }).catch((err)=>{
+        let message="查無此人"
+        showperson.textContent=message
+        showresult.append(showperson)
+        console.log('There was an error!', err);
+    });
+
+})
+
+inputchange.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let changename=document.querySelector(".changename").value
+    let change =document.querySelector(".change")
+    let changemessage = document.querySelector(".changemessage")
+    console.log(changename)
+    fetch("http://127.0.0.1:3000/api/members",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"},
+        body:JSON.stringify({ name:changename})
+    }).then((response)=>{
+        return response.json();
+    }).then((jsonData)=>{
+        if(jsonData["ok"]=="true"){//練習用[]取值
+        let titlename=document.querySelector(".titlename")
+        change.textContent = "更新成功"
+        titlename.textContent= `恭喜您${changename}，成功登入系統`
+        changemessage.append(change)
+        console.log("success");
+        } 
+        else{change.textContent = "請輸入其他姓名或重新登入"
+        changemessage.append(change)}
+    }).catch((err)=>{
+        console.log('There was an error!', err);
+    })
+})
